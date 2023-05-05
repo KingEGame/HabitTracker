cf1d77de5f1dd7de786eac0e79689d982333cb16
KINGWAR <damirov.ilian@gmail.com>
2023-05-05 22:25:14 +0600

    adding project files and dependencies

---
+++ src/test/java/kg/habittracker/project/HabitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.habittracker.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
